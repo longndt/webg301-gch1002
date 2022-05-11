e861217291a778edfe6452b9534a859f7bd537d7
longndt <longndt226@gmail.com>
2022-05-11 11:23:26 +0700

    crud

---
+++ crud123/src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        //sử dụng vòng lặp for để add được nhiều object (record) cùng lúc
+        for ($i=1; $i<=20 ; $i++) {
+            //tạo variable đại diện cho 1 object Article
+            $article = new Article;
+            //sử dụng setter để set dữ liệu cho object
+            $article->setLength(rand(5,30));  //random 1 số trong khoảng 5 đến 30
+            $article->setName("Article " . $i);
+            $article->setDate(\DateTime::createFromFormat('Y-m-d','2022-05-11'));
+            //lưu dữ liệu vào DB
+            $manager->persist($article);
+        }
+
+        //confirm thao tác add dữ liệu vào DB
+        $manager->flush();
+    }
+}
---
+++ crud123/src/Entity/Article.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    private $length;
+
+    #[ORM\Column(type: 'date')]
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLength(): ?int
+    {
+        return $this->length;
+    }
+
+    public function setLength(int $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
