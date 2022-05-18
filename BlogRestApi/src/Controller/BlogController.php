67711eef5e64101f0b762d0fb99be9b6d49d63c7
longndt <longndt226@gmail.com>
2022-05-18 08:33:28 +0700

    api

--- BlogRestApi/src/Controller/BlogController.php
+++ BlogRestApi/src/Controller/BlogController.php
@@ -20,6 +20,7 @@ class BlogController extends AbstractController
     public function viewAllBlog (BlogRepository $blogRepository) {
         //lấy dữ liệu từ bảng Blog trong database
         $blogs = $blogRepository->findAll();
+        //$blogs = $this->getDoctrine()->getRepository(Blog::class)->findAll();
         //convert dữ liệu thành chuẩn json (api)
         $json = $this->serializerInterface->serialize($blogs,'json');
         //return 1 response chứa dữ liệu theo format json
@@ -33,6 +34,12 @@ class BlogController extends AbstractController
     #[Route('/{id}', methods: ['GET'], name: 'view_blog_by_id')]
     public function viewBlogById ($id, BlogRepository $blogRepository) {
         $blog = $blogRepository->find($id);
+        //kiểm tra xem dữ liệu có tồn tại trong DB không
+        //TH1: không tồn tại
+        if ($blog == null) {
+            return new Response("Blog not found", Response::HTTP_NOT_FOUND); //code = 404
+        }
+        //TH2: có tồn tại
         //convert dữ liệu thành chuẩn xml (api)
         $xml = $this->serializerInterface->serialize($blog,'xml');
         //return 1 response chứa dữ liệu theo format xml
@@ -41,4 +48,19 @@ class BlogController extends AbstractController
             'content-type' => 'application/xml'
         ]);
     }
+
+    //DELETE FROM Blog WHERE id = $id
+    #[Route('/{id}', methods: ['DELETE'], name: 'delete_blog')]
+    public function deleteBlog ($id, BlogRepository $blogRepository) {
+        $blog = $blogRepository->find($id);
+        if ($blog != null) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($blog);
+            $manager->flush();
+            return new Response(null, Response::HTTP_NO_CONTENT);
+        } else {
+            return new Response("Blog not found", 404);
+        }
+    }
+    
 }   
