d8c5d4af385a29cfde15f42bb72a67d33f4c74b1
longndt <longndt226@gmail.com>
2022-05-23 09:02:11 +0700

    form

---
+++ form/src/Controller/StudentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Student;
+use App\Repository\StudentRepository;
+use Doctrine\DBAL\Types\FloatType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    #[Route('/', name: 'view_students')]
+    public function viewStudents(ManagerRegistry $managerRegistry, StudentRepository $studentRepository) {
+        //Cách 1 (không cần thêm parameter khi khai báo function)
+        $students1 = $this->getDoctrine()->getRepository(Student::class)->findAll();
+        //Cách 2 (cần có parameter ManagerRegistry khi khai báo function)
+        $students2 = $managerRegistry->getRepository(Student::class)->findAll();
+        //Cách 3 (cần có parameter của Repository khi khai báo function)
+        $students3 = $studentRepository->findAll();
+        //Note: chỉ sử dụng 1 cách để lấy dữ liệu từ DB
+        return $this->render("student/index.html.twig",
+        [
+            'students' => $students3
+        ]);
+    }
+
+    //Cách 1: tạo form trực tiếp từ Controller (not recommend)
+    #[Route('/add', name: 'add_student')]
+    public function addStudent(Request $request, ManagerRegistry $managerRegistry) {
+        //B1: tạo 1 object cho entity để lưu dữ liệu nhập từ form
+        $student = new Student;
+        //B2: tạo form sử dụng hàm createFormBuilder()
+        $form = $this->createFormBuilder($student)
+                      ->add("Name", TextType::class)   //HTML: input type="text"
+                      ->add("Age", IntegerType::class) 
+                      ->add("Grade", FloatType::class)
+                      ->add("Add", SubmitType::class)   //HTML: input type="submit"
+                      ->getForm();
+        //B3: handle request cho form
+        $form->handleRequest($request);
+        //B4: kiểm tra xem form đã được submit và dữ liệu đã được validate chưa
+        if ($form->isSubmitted() && $form->isValid()) {
+            //lưu dữ liệu từ entity vào DB
+            //$manager = $this->getDoctrine()->getManager();
+            //Note: nếu $this->getDoctrine() bị lỗi thì dùng ManagerRegistry để thay thế
+            $manager = $managerRegistry->getManager();
+            $manager->persist($student);
+            $manager->flush();
+        }
+        //B5: render ra view chứa form
+        //Cách 1: dùng hàm render() & createView()
+        return $this->render("student/add.html.twig",
+            [
+                'studentForm' => $form->createView()
+            ]
+        );
+    }
+}
---
+++ form/src/Entity/Student.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudentRepository::class)]
+class Student
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    private $age;
+
+    #[ORM\Column(type: 'float')]
+    private $grade;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getGrade(): ?float
+    {
+        return $this->grade;
+    }
+
+    public function setGrade(float $grade): self
+    {
+        $this->grade = $grade;
+
+        return $this;
+    }
+}
